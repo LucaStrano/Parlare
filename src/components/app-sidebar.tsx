eac2438eb2e8676a7ed9b622f23f39da1741fba0
Luca Strano <lucastrano2111@icloud.com>
2025-05-12 23:14:23 +0200

    Add search input to sidebar for chat filtering

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -11,8 +11,10 @@ import {
 } from "~/components/ui/sidebar"
 
 import { useSidebar } from "~/components/ui/sidebar"
+import { Input } from "~/components/ui/input"
 
-import { Settings } from "lucide-react"
+
+import { Search, Settings } from "lucide-react"
 
 import {chatsAtom, isSidebarOpenAtom} from "~/atoms/main-atoms"
 import { useAtomValue, useSetAtom } from "jotai"
@@ -40,12 +42,22 @@ export default function AppSidebar() {
       <SidebarHeader className="mt-11">Chats</SidebarHeader>
 
       <SidebarContent>
+
+        {/* SEARCH GROUP */}
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <Input type="text" placeholder="Search Chats..." className="h-8 italic">
+            </Input>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* CHATS GROUP */}
         <SidebarGroup />
             <SidebarGroupContent>
               <SidebarMenu>
                 {chats.map( (chat) =>
                   (
-                    <SidebarMenuItem key={chat.id} className=" text-lg mt-1">
+                    <SidebarMenuItem key={chat.id} className=" text-lg">
                       <SidebarMenuButton asChild>
                         <span>{chat.name}</span>
                       </SidebarMenuButton>
