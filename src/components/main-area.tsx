db14412f946f7b7b76c056b929042cbd8c605299
Luca Strano <lucastrano2111@icloud.com>
2025-05-13 22:49:55 +0200

    Replace Input with Textarea component in MainArea

--- src/components/main-area.tsx
+++ src/components/main-area.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
 
 export default function MainArea(){
     return (
         <div className="flex-1 h-full flex flex-col border border-red-500">
             <h1> main area </h1>
-            <Input className="relative mt-auto mb-4 overflow-x-scroll"></Input>
+            <Textarea rows={2} className="resize-none relative mt-auto mb-4"></Textarea>
         </div>
     )
 }
