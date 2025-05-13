a936360d8bb4314ba4566be3f3b9cd995a382712
Luca Strano <lucastrano2111@icloud.com>
2025-05-14 00:12:33 +0200

    Add input-area div with send button

--- src/components/main-area.tsx
+++ src/components/main-area.tsx
@@ -1,5 +1,7 @@
 import { Textarea } from "~/components/ui/textarea";
 import { useState, ChangeEvent, useRef } from "react";
+import { Send, Truck } from "lucide-react";
+import { Button } from "~/components/ui/button";
 
 export default function MainArea(){
 
@@ -15,17 +17,24 @@ export default function MainArea(){
     }
 
     return (
-        <div className="flex-1 h-full flex flex-col border border-red-500">
+        <div className="flex-1 h-full flex flex-col bg-slate-100 border border-red-500">
             <h1> main area </h1>
-            <Textarea
-                ref={areaRef}
-                value={areaValue} 
-                onChange={handleAreaChange} 
-                rows={1}
-                className="resize-none relative mt-auto mb-4 max-h-24 overflow-y-auto placeholder:italic"
-                placeholder="Write Anything..."
-            >
-            </Textarea>
+            <div id="input-area" className="flex flex-col mt-auto mb-4 mx-4 rounded-md shadow-md bg-white select-none">
+                <Textarea
+                    ref={areaRef}
+                    value={areaValue} 
+                    onChange={handleAreaChange} 
+                    rows={1}
+                    className="resize-none relative max-h-24 overflow-y-auto border-none placeholder:italic"
+                    placeholder="Write Anything..."
+                >
+                </Textarea>
+                <div onClick={() => {areaRef.current?.focus()}} className="flex flex-row justify-end">
+                    <Button disabled={areaValue.trim() === ""} id="new-chat" variant="ghost" size="icon" className="h-7 w-7 m-2">
+                        <Send width={22} height={22}></Send>
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
