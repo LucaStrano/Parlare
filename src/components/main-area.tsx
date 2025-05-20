86b3077d97f95335574dbd04fa579dae002c05d4
Luca Strano <lucastrano2111@icloud.com>
2025-05-20 12:15:15 +0200

    Add message send functionality

--- src/components/main-area.tsx
+++ src/components/main-area.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, useRef } from "react";
+import { useState, ChangeEvent, useRef, KeyboardEvent } from "react";
 
 import { Textarea } from "~/components/ui/textarea";
 import { Button } from "~/components/ui/button";
@@ -9,6 +9,7 @@ import { useAtom } from "jotai";
 import { chatMessagesAtom } from "~/atoms/main-atoms";
 
 import { Send } from "lucide-react";
+import { ChatMessage } from "~/types/main-types";
 
 export default function MainArea(){
 
@@ -52,6 +53,27 @@ export default function MainArea(){
         }
     }
 
+    function handleOnKeyDown(event: KeyboardEvent<HTMLTextAreaElement>){
+        if(event.key==="Enter" && !event.shiftKey){
+            event.preventDefault();
+            sendMessage();
+        }
+    }
+
+    function sendMessage(){
+        const msg = areaValue.trim();
+        if (!msg) return;
+
+        const msgObj : ChatMessage = {
+            id: Date.now().toString(),
+            type: "user",
+            date: new Date(),
+            content: msg,
+        };
+        setChatMessages((prev) => [ ...(prev ?? []), msgObj ]);
+        setAreaValue("");
+    }
+
     return (
         <div className="flex-1 h-full flex flex-col bg-slate-100">
             <ScrollArea className="flex-auto mb-1 border border-red-500">
@@ -70,14 +92,21 @@ export default function MainArea(){
                 <Textarea
                     ref={areaRef}
                     value={areaValue} 
-                    onChange={handleAreaChange} 
+                    onChange={handleAreaChange}
+                    onKeyDown={handleOnKeyDown}
                     rows={1}
                     className="resize-none mt-1 relative max-h-24 overflow-y-auto border-none placeholder:italic transition-[height] duration-100 ease-out"
                     placeholder="Wite Anything..."
                 >
                 </Textarea>
                 <div onClick={() => {areaRef.current?.focus()}} className="flex flex-row justify-end">
-                    <Button disabled={areaValue.trim() === ""} id="new-chat" variant="ghost" size="icon" className="h-7 w-7 m-2">
+                    <Button
+                        onClick={sendMessage}
+                        disabled={areaValue.trim() === ""}
+                        id="new-chat"
+                        variant="ghost"
+                        size="icon"
+                        className="h-7 w-7 m-2">
                         <Send width={22} height={22} className="rotate-45 -translate-x-[4px]"></Send>
                     </Button>
                 </div>
